e61a7f966d4da04195504debb93a54a4a567e708
Bor333 <sajblerboris@gmail.com>
2024-08-20 07:12:51 +0700

    pretty fixes

--- templates/index.php
+++ templates/index.php
@@ -1,13 +1,13 @@
 <?php foreach ($usersResult as $user): ?>
-    <p>Имя: <?= $user['first_name'] ?></p>
-    <p>Фамилия: <?= $user['last_name'] ?></p>
-    <p>Пол: <?= $user['gender'] ?></p>
-    <p>Дата рождения: <?= $user['birth_date'] ?></p>
-    <form method="post" action="/delete">
+    <p>First name: <?= $user['first_name'] ?></p>
+    <p>Last name: <?= $user['last_name'] ?></p>
+    <p>Gender: <?= $user['gender'] ?></p>
+    <p>Birth date: <?= $user['birth_date'] ?></p>
+    <form method="post" action="/delete" style="display: inline-block">
         <input hidden type="text" name="id" value="<?= $user['id'] ?>">
         <input type="submit" value="delete">
     </form>
-    <form method="post" action="/update">
+    <form method="post" action="/update" style="display: inline-block">
         <input hidden type="text" name="id" value="<?= $user['id'] ?>">
         <input hidden type="text" name="first_name" value="<?= $user['first_name'] ?>">
         <input hidden type="text" name="last_name" value="<?= $user['last_name'] ?>">
